fc277c310f9ae699ab9454948efc15371884907b
victor <victorandrej@outlook.com>
2022-07-21 17:32:10 -0300

    inclusao de um erro de compilacao para forcar o usuario incluir o token em teste

--- src/test/java/br/com/victorandrej/locationdetector/ApiToken.java
+++ src/test/java/br/com/victorandrej/locationdetector/ApiToken.java
@@ -1,5 +1,5 @@
 package br.com.victorandrej.locationdetector;
 
 public class ApiToken {
-	public static final String TOKEN = "076c259795cd978d450febebd931cf92";
+	public static final String TOKEN 
 }
